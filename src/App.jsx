0a964844f2c051c9f296c036fbb23245c9a49320
Stanislav Angelov <stannyweb@gmail.com>
2024-01-19 08:06:27 +0200

    Added CSS Module

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Homepage from './pages/Homepage'
+import Products from './pages/Products'
+import Pricing from './pages/Pricing'
+import NotFound from './pages/NotFound'
+import AppLayout from './pages/AppLayout'
+// import PageNav from './components/PageNav'
+
 export default function App () {
-  return <div>WorldWise</div>
+  return (
+    <div>
+      <BrowserRouter>
+        {/* <PageNav /> */}
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='products' element={<Products />} />
+          <Route path='pricing' element={<Pricing />} />
+          <Route path='app' element={<AppLayout />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
 }
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+import PageNav from '../components/PageNav'
+
+export default function NotFound () {
+  return (
+    <div>
+      <PageNav />
+      NotFound
+    </div>
+  )
+}
